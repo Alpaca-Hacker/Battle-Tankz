b840c59e8b578826d9e1e60e43afde3bc6d82424
Paul Gent <Paul@GentMail.com>
2017-10-03 20:34:29 +0100

    Got the turret rotation working

--- BattleTankz/Source/BattleTankz/Private/Turret.cpp
+++ BattleTankz/Source/BattleTankz/Private/Turret.cpp
@@ -1,10 +1,24 @@
 // Copyright P. Gent 2017
 
 #include "Turret.h"
-
+#include "Engine/World.h"
 
 bool UTurret::Rotate(float RelativeSpeed)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Turret wants to move %f"), RelativeSpeed);
-	return false;
+	if (RelativeSpeed == 0)
+	{
+		return false;
+	}
+	RelativeSpeed = FMath::Clamp(RelativeSpeed, -1.0f, 1.0f);
+	const auto RotationChange = RelativeSpeed * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
+	auto RawRotationChange = RelativeRotation.Yaw + RotationChange;
+	//RawElevationChange = FMath::Clamp(RawElevationChange, MinElevation, MaxElevation);
+	if (RawRotationChange == 0)
+	{
+		return false;
+	}
+	//UE_LOG(LogTemp, Warning, TEXT("Turret wants to move %f"), RelativeSpeed);
+	SetRelativeRotation(FRotator(0, RawRotationChange, 0));
+	return true;
+
 }
